535ce1161b88d8134dea7d8409a226ed081d87a0
amane.toda <toda.amane@gmail.com>
2021-10-07 16:26:02 +0100

    Updated solution. Issue that it doesn't find every possible solution given a starting step size

--- 3_grandest_staircase/solution.py
+++ 3_grandest_staircase/solution.py
@@ -8,19 +8,24 @@ def solution(n):
         if attempt_outcome == 0:
             print("Staircase is possible")
             return True
-        elif next_attempt == 1: #Cannot attempt building further
+        elif next_attempt <= 1: #Cannot attempt building further
             print("Staircase is impossible")
             return False
         elif attempt_outcome <0:
-            print("Starting with {} bricks, my previous step was {}. I had most recently attempted {}".format(bricks_left,previous_step,next_attempt))
+            print("It's impossible to build {}".format(next_attempt))
             build_step(previous_step,next_attempt-1,bricks_left)
         else:
-            build_step(next_attempt-1,next_attempt-1,bricks_left - (previous_step-1))
+            print("It's possible to build {}".format(next_attempt))
+            build_step(next_attempt,next_attempt-1,bricks_left - (previous_step-1))
     
-    starting = 2
-    build_step(starting,starting-1,n-starting)
-    # for every possible starting n: #Could optimise to find which n can be started at (e.g. 1 cannot be the starting step)
-    #     if can_build_staircase is True:
-    #         num_solutions +=1
+    # starting = 2
+    # build_step(starting,starting-1,n-starting)
+    for starting_step_height in reversed(range(2,n)): #Could optimise to find which n can be started at (e.g. 1 cannot be the starting step)
+        print("From {} bricks, with a starting step of {}".format(n,starting_step_height))
+        bricks_left = n - starting_step_height
+        is_staircase_possible = build_step(starting_step_height,starting_step_height-1,bricks_left)
+        if is_staircase_possible is True:
+            print("From {} bricks, with a starting step of {}, the staircase is possible\n".format(n,starting_step_height))
+            num_solutions +=1
 
-solution(6)        
+solution(10)        
