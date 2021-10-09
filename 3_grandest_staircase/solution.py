819e8a1b27309c91da628d9abd29cae6ef527764
amane.toda <toda.amane@gmail.com>
2021-10-09 14:26:42 +0100

    Working solution, pre dynamic programming implementation

--- 3_grandest_staircase/solution.py
+++ 3_grandest_staircase/solution.py
@@ -1,45 +1,34 @@
-import time
-
 def solution(n):
     num_solutions = 0
-    stored_answers = []
 
-    # def num_staircase(num_bricks):
-    #     # num_answers = 0
-    #     # print("Finding the number of solutions if {} is the number of bricks".format(num_bricks))
-    #     if num_bricks <=2:
-    #         # print("Not possible to create staircases from {} bricks".format(num_bricks))
-    #         return 0
-    #     else:
-    #         num_answers = (num_bricks - 1)//2
-    #         stored_answers.append([num_bricks,num_answers])
-    #         # print("Possible to split {} bricks into {} length 2 staircases".format(num_bricks,num_answers)) 
-    #         # print("Need to explore creating staircases for {} parts".format(range(1,num_bricks//2 + 1)))   
-    #         # time.sleep(5) #TODO: Used for testing, delete if not needed
-    #         return num_answers + sum(num_staircase(x) for x in range(1,num_bricks//2 + 1))
-    #         # total_answers += num_answers
-            
-    #         # for subset in reversed(range(1,num_bricks//2 + 1)):
-    #             # print("Exploring how many staircases can be made from {} bricks".format(subset))
-    #             # return num_staircase(subset,total_answers)
-
-    def combinations_with_end_step(num_bricks,furthest_step,total_answers):
-        print("Finding how many staircases can be built with {} bricks left, given the furthest step is of height {}".format(num_bricks,furthest_step))
-        if num_bricks <= furthest_step:
-            print("Impossible")
+    def num_combinations_given_end_step(num_bricks,furthest_step,total_answers):
+        # print("Finding how many staircases can be built with {} bricks left, given the furthest step is of height {}".format(num_bricks,furthest_step))
+        if num_bricks <= furthest_step: #Cannot build as steps must be larger than the previous one
+            # print("Impossible")
             return total_answers        
         else:            
-            total_answers += 1
-            print("Solution possible, now total answers is {}".format(total_answers))
-            for next_step in range(furthest_step+1,num_bricks-furthest_step):
-                print("From {} bricks, trying to build a step of {}".format(num_bricks,next_step))
-                total_answers = combinations_with_end_step(num_bricks-next_step,next_step,total_answers)
+            total_answers += 1 #Can always build a step that is num_bricks tall
+            # print("Solution possible, now total answers is {}".format(total_answers))
+            for next_step in range(furthest_step+1,num_bricks-furthest_step): #Incrementally build the next step, reducing number of total bricks accordingly.
+                # print("From {} bricks, trying to build a step of {}".format(num_bricks,next_step))
+                total_answers = num_combinations_given_end_step(num_bricks-next_step,next_step,total_answers)
             return total_answers
-            # return sum(combinations_with_end_step(num_bricks-next_step,next_step,total_answers) for next_step in range(furthest_step+1,num_bricks-furthest_step))
+            # return sum(num_combinations_given_end_step(num_bricks-next_step,next_step,total_answers) for next_step in range(furthest_step+1,num_bricks-furthest_step))
     
-    final_step = 2
-    num_solutions = combinations_with_end_step(n-final_step,final_step,0)  
+    def num_staircase(num_starting_bricks,staircase_solutions):
+        # print("Finding out how many staircases can be built from {} bricks\n".format(num_starting_bricks))
+        for first_step in range(1,n//2 + 1):
+            new_solutions = 0
+            new_solutions += num_combinations_given_end_step(num_starting_bricks - first_step,first_step,0)
+            
+            staircase_solutions += new_solutions
+            # print("From {} starting step, it's possible to create {} staircases from the remaining {} bricks. There are now {} total solutions\n".format(first_step,new_solutions,num_starting_bricks-first_step,staircase_solutions))
+        return staircase_solutions
+
+    # final_step = 2
+    # num_solutions = num_combinations_given_end_step(n-final_step,final_step,0)  
+    num_solutions = num_staircase(n,num_solutions)
     return num_solutions
 
-test = solution(9)        
+test = solution(5)        
 print("{} is the answer".format(test))
