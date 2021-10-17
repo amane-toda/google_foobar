8c1b713d28459beeb92d9a69b5fe63f7f8676ad7
amane.toda <toda.amane@gmail.com>
2021-10-17 16:38:11 +0100

    Update how adjacent cell is checked to improve readability

--- 3_bunnies_escape/solution.py
+++ 3_bunnies_escape/solution.py
@@ -8,77 +8,64 @@ def solution(map):
             or col < 1
         )
         if is_invalid is True: #Position doesn't exist
-            # print("Row is {} and column is {} but map is {}".format(row,col,current_map))
             return None 
         else:
-            return current_map[row-1][col-1]
+            return current_map[row-1][col-1] # -1 as row/column count starts at 1, while index starts at 0
     
     def deep_copy_map(input_map):
-        num_rows = len(input_map)
-        num_cols = len(input_map[0])
-
         map_copy = [row_lists[:] for row_lists in map] #Perform a deep copy of the list of lists, to prevent nested lists pointing to the same object
-        
         return map_copy
 
     def find_solutions(input_map, num_rows,num_cols):
         
-        #Create input_map to store counter of moves
+        #Create map to store counter of moves to get there
         move_counter_map = deep_copy_map(input_map)
         for row in range(num_rows):
             for col in range(num_cols):
                 move_counter_map[row][col] = 0
-    
-        #Backup solution
-        # print("Original map is {}, new map is {}".format(input_map, move_counter_map))
         
         # First_move always at row 1 and column 1
         move_counter_map[0][0] = 1
-        # Create list of 
         starting_position_list = [[1,1]] 
     
-        while starting_position_list: #While there is still a position to check
+        while starting_position_list: #While there is still a position that is reachable to check
             # print("\nStarting with list of positions to check as {}. Move counter map is {}".format(starting_position_list,move_counter_map))
-            current_pos = starting_position_list.pop(0)
+            current_pos = starting_position_list.pop(0) #Take out the first position to check
+
+            #Store row and column values
             current_row = current_pos[0]
-            current_col = current_pos[1]        
+            current_col = current_pos[1]
+            #Check what the current counter to reach this spot is.        
             current_counter = check_position(move_counter_map,current_row,current_col)
-    
             # print("Popped {} leaving the list as {}. The current position has a counter of {}".format(current_pos,starting_position_list,current_counter))
     
-            adjacent_cells = [
+            # Create list of possible positions to move to. Can either move up/down or left/right
+            adjacent_cells = [ 
                     [current_row+1,current_col],[current_row-1,current_col], 
                     [current_row,current_col+1],[current_row,current_col-1]
             ]
+
+            #For every possible move
             for adjacent_cell in adjacent_cells:
+                #Store row and column for this cell
                 new_row = adjacent_cell[0]
                 new_col = adjacent_cell[1]
-                # print("Checking {} from list of adjacent cells".format(adjacent_cell))
                 check_adjacent_cell = check_position(input_map, new_row,new_col) 
-                # print(check_adjacent_cell)
-                if check_adjacent_cell is None: #If adjacent cell position doesn't exist, loop over to next step
-                    # print("This cell position {} is not valid".format(adjacent_cell))
-                    continue
-    
-                if check_adjacent_cell == 1: #Blocked by a wall
-                    # print("This cell position {} is a wall so is not valid".format(adjacent_cell))
-                    continue
-    
-                dist = current_counter + 1
-    
-                check_adjacent_cell_dist = check_position(move_counter_map, new_row,new_col) 
-    
-                # print("Made it to distance calculation. For position {}, distance is {}. Adjace cell dist is {} ".format(adjacent_cell,dist,check_adjacent_cell_dist))
-                if dist < check_adjacent_cell_dist or check_adjacent_cell_dist == 0: #If the new distance is less than the current one, or no distnace has been calculated yet
-                    move_counter_map[new_row-1][new_col-1] = dist
-                    starting_position_list.append(adjacent_cell)
-                    # print("Found a new shortest distance, new map of counters is {}.".format(move_counter_map))
+
+                if check_adjacent_cell is not None and check_adjacent_cell == 0: #If adjacent cell position exists and is not a wall
+                    dist = current_counter + 1  # Reachable by current counter + 1 to move to adjacent position
+        
+                    check_adjacent_cell_dist = check_position(move_counter_map, new_row,new_col)  #Check current recorded distance for this cell
+        
+                    # print("Made it to distance calculation. For position {}, distance is {}. Adjace cell dist is {} ".format(adjacent_cell,dist,check_adjacent_cell_dist))
+                    if dist < check_adjacent_cell_dist or check_adjacent_cell_dist == 0: #If the new distance is less than the current one, or no distnace has been calculated yet
+                        move_counter_map[new_row-1][new_col-1] = dist
+                        starting_position_list.append(adjacent_cell)
+                        # print("Found a new shortest distance, new map of counters is {}.".format(move_counter_map))
         return move_counter_map[num_rows-1][num_cols-1]
     
     map_rows = len(map)
     map_cols = len(map[0])
-    min_possible_moves = map_rows + map_cols - 1
-    # print("{} has {} rows and {} columns. The minimum possible moves for the solution is {}".format(map, map_rows, map_cols,min_possible_moves))
     min_moves = 0
 
     #Find solution, if no walls are removed
