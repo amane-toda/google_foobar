cf5dea433d19ce03ded4e79926c088f36ed6b0b1
amane.toda <toda.amane@gmail.com>
2021-10-17 15:12:39 +0100

    Create a random test case generator

--- 3_bunnies_escape/solution.py
+++ 3_bunnies_escape/solution.py
@@ -1,3 +1,5 @@
+import random
+
 def solution(map):
     #Map is formatted with map[n][m] being wall at row n and column m    
     def check_position(current_map,row,col):
@@ -38,13 +40,13 @@ def solution(map):
         starting_position_list = [[1,1]] 
     
         while starting_position_list: #While there is still a position to check
-            print("\nStarting with list of positions to check as {}. Move counter map is {}".format(starting_position_list,move_counter_map))
+            # print("\nStarting with list of positions to check as {}. Move counter map is {}".format(starting_position_list,move_counter_map))
             current_pos = starting_position_list.pop(0)
             current_row = current_pos[0]
             current_col = current_pos[1]        
             current_counter = check_position(move_counter_map,current_row,current_col)
     
-            print("Popped {} leaving the list as {}. The current position has a counter of {}".format(current_pos,starting_position_list,current_counter))
+            # print("Popped {} leaving the list as {}. The current position has a counter of {}".format(current_pos,starting_position_list,current_counter))
     
             adjacent_cells = [
                     [current_row+1,current_col],[current_row-1,current_col], 
@@ -53,15 +55,15 @@ def solution(map):
             for adjacent_cell in adjacent_cells:
                 new_row = adjacent_cell[0]
                 new_col = adjacent_cell[1]
-                print("Checking {} from list of adjacent cells".format(adjacent_cell))
+                # print("Checking {} from list of adjacent cells".format(adjacent_cell))
                 check_adjacent_cell = check_position(input_map, new_row,new_col) 
                 # print(check_adjacent_cell)
                 if check_adjacent_cell is None: #If adjacent cell position doesn't exist, loop over to next step
-                    print("This cell position {} is not valid".format(adjacent_cell))
+                    # print("This cell position {} is not valid".format(adjacent_cell))
                     continue
     
                 if check_adjacent_cell == 1: #Blocked by a wall
-                    print("This cell position {} is a wall so is not valid".format(adjacent_cell))
+                    # print("This cell position {} is a wall so is not valid".format(adjacent_cell))
                     continue
     
                 dist = current_counter + 1
@@ -72,7 +74,7 @@ def solution(map):
                 if dist < check_adjacent_cell_dist or check_adjacent_cell_dist == 0: #If the new distance is less than the current one, or no distnace has been calculated yet
                     move_counter_map[new_row-1][new_col-1] = dist
                     starting_position_list.append(adjacent_cell)
-                    print("Found a new shortest distance, new map of counters is {}.".format(move_counter_map))
+                    # print("Found a new shortest distance, new map of counters is {}.".format(move_counter_map))
         return move_counter_map[num_rows-1][num_cols-1]
     
     map_rows = len(map)
@@ -84,6 +86,7 @@ def solution(map):
     #Find solution, if no walls are removed
     new_solution = find_solutions(map,map_rows,map_cols) #Find solution, if no walls are removed
     if new_solution > 0: #is solvable
+        print("From map {}, found solution {}".format(map,new_solution))
         min_moves = new_solution
     
     #Find solution, if walls are not removed
@@ -96,6 +99,7 @@ def solution(map):
                 new_solution = find_solutions(map_copy,map_rows,map_cols) #Find the solution
 
                 if new_solution > 0 and (new_solution < min_moves or min_moves == 0): #If a solution is found and the new solution is shorter than the existing one, or existing one doesn't exist
+                    print("From map {}, found solution {}".format(map_copy,new_solution))
                     min_moves = new_solution
                 
                 if min_moves == min_possible_moves: #Shortest solution found
@@ -108,8 +112,23 @@ test_case_1 = [[0, 1, 1, 0], [0, 0, 0, 1], [1, 1, 0, 0], [1, 1, 1, 0]]
 test_case_2 = [[0, 0, 0, 0, 0, 0], [1, 1, 1, 1, 1, 0], [0, 0, 0, 0, 0, 0], [0, 1, 1, 1, 1, 1], [0, 1, 1, 1, 1, 1], [0, 0, 0, 0, 0, 0]]
 
 # num_moves = solution(test_case_1)
-num_moves = solution(test_case_2)
-
+# num_moves = solution(test_case_2)
 print(num_moves)
-# print(type(test_case_1))
-# print("I am currently at row 1 and column 1, this has value {}".format(test_case_1[0][1]))
+
+def random_test_case_generator(num_rows,num_cols):
+    generated_map = []
+    for i in range(20):
+        temp_list = []
+        for j in range(20):
+            rand = random.uniform(0,1)
+            temp_list.append(int(round(rand)))
+        generated_map.append(temp_list)
+    return generated_map
+
+test_case = random_test_case_generator(20,20)
+
+print(test_case)
+
+
+num_moves = solution(test_case)
+
